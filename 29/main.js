9f0667f8f1b87f47b84f65810925547bf7a7adf2
Cheburek <91902727+Albina2611@users.noreply.github.com>
2022-01-10 17:56:35 +0200

    Initial commit

---
+++ 29/main.js
@@ -0,0 +1,55 @@
+// let name = 'Danil'
+// let secondname = 'Seleznyov'
+// let age = prompt('your age')
+// alert(`Hello, my name is ${name} ${secondname} my age is ${age}`)
+// alert(55*55)
+
+// let a = prompt(`What width of the one side of square?`)
+// let b = prompt('Какая длина стороны квадрата')
+// alert(4*a)
+// alert(b*b)
+
+
+// let c = prompt(`What is the width of smaller side of rectangle?`)
+// let d = prompt(`What is the width of bigger side of rectangle?`)
+// alert(c*d)
+// alert((c+d)*2)
+
+// let e = prompt(`Какой диаметр окружности?`)
+// alert(e*3.14)
+
+
+// let a = prompt(`Какая длина ребра куба?`)
+// alert(a*a*a)
+
+
+// let a = prompt(`Какая длина ребра №1?`)
+// let b = prompt(`Какая длина ребра №2?`)
+// let c = prompt(`Какая длина ребра №3?`)
+// alert(a*b*c)
+// alert(a*b + b*c + a*c)
+
+
+// let R = prompt(`Какой радиус круга?`)
+// alert(2*3.14*R)
+
+
+// let a = prompt(`Number a?`)
+// let b = prompt(`Number b?`)
+// alert((1*a + 1*b)/2)
+
+
+// let a = prompt(`Number a?`)
+// let b = prompt(`Number b?`)
+// alert((a*b))
+// alert (Math.sqrt(a*b))
+
+
+// let a = prompt(`Number a?`)
+// let b = prompt(`Number b?`)
+// alert(1*a + 1*b)
+// alert(a - b)
+// alert(a * b)
+// alert(1*a / 1*b)
+
+
